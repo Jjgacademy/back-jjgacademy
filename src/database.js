3d8dfd03d8b10abcf6764242ae50481439be3c24
JohanP151 <jas.pavon@yavirac.edu.ec>
2026-02-09 15:17:41 -0500

    cambios en el database.js

--- src/database.js
+++ src/database.js
@@ -12,5 +12,12 @@ export const sequelize = new Sequelize(
     port: Number(process.env.DB_PORT),
     dialect: "postgres",
     logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
